000329a7e8000a2edb40fbe8331e72ff3f0ba33e
Thomas <thomi.reynoso@hotmail.com>
2019-10-05 20:21:25 -0300

    Uso de libreria MUX para manejar Rutas

--- API-Rest/main.go
+++ API-Rest/main.go
@@ -4,23 +4,32 @@ import (
 		"fmt"
 		"net/http"
 		"log"  //libreria para capturar errores y mostrarlos por la consola
+		"github.com/gorilla/mux"
 		) 
 
 func main() {
-	//Para crear una Ruta. le paso una func anonima (Callback)
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		//Aca pongo lo que devuelve el server en esta ruta
-		
-		//Le paso el Writer, donde tiene que escribir 
-		fmt.Fprintf(w, "Hola mundo desde mi nuevo servidor web con GO")
-	})
+	
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/contacto", Contact)
 
 
 	/*Metodo para que el servidor se levante en el puerto 8080 y 
-	escuche peticiones*/ 
-	server := http.ListenAndServe(":8080", nil)
+	escuche peticiones. Recibe el objeto "Router", el cual tendra TODAS las 
+	config de router y las distintas rutas que establezca*/ 
+	server := http.ListenAndServe(":8080", router)
 	log.Fatal(server)
 
 
 }
 
+func Index(w http.ResponseWriter, r *http.Request) {
+		//Aca pongo lo que devuelve el server en esta ruta
+		
+		//Le paso el Writer, donde tiene que escribir 
+		fmt.Fprintf(w, "Hola mundo desde mi nuevo servidor web con GO")
+}
+
+func Contact(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Bienvenido a la pagina de Contactos")
+}
